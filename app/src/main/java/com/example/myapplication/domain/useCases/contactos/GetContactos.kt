e2c90dcaf2308398d921b4358512b814d117c9fe
Dani <119665698+S4nxez@users.noreply.github.com>
2024-10-04 11:12:11 +0200

    basic app

--- app/src/main/java/com/example/myapplication/domain/useCases/contactos/GetContactos.kt
+++ app/src/main/java/com/example/myapplication/domain/useCases/contactos/GetContactos.kt
@@ -2,6 +2,6 @@ package com.example.myapplication.domain.useCases.contactos
 
 import com.example.myapplication.data.Repository
 
-class GetContactos( private val repo : Repository){
-    operator fun invoke() = repo.getContactos()
+class GetContactos(){
+    operator fun invoke() = Repository.getContactos()
 }
